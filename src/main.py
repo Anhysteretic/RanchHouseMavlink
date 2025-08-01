4b52fe9507cf1a1125597d431b997fec9163785b
David <s@anhysteretic.com>
2025-08-01 15:57:02 -0400

    upload even more more more

--- src/main.py
+++ src/main.py
@@ -21,11 +21,11 @@ async def run():
             print(f"-- Connected to drone!")
             break
     
-    print("Waiting for drone to have a global position estimate...")
-    async for health in drone.telemetry.health():
-        if health.is_global_position_ok and health.is_home_position_ok:
-            print("-- Global position estimate OK")
-            break
+    # print("Waiting for drone to have a global position estimate...")
+    # async for health in drone.telemetry.health():
+    #     if health.is_global_position_ok and health.is_home_position_ok:
+    #         print("-- Global position estimate OK")
+    #         break
 
     print("-- Arming")
     await drone.action.arm()
